4f9edb2b7caffedc50f28817cc2fa14537a54d1d
Pmaichpm <pmaich.pm@gmail.com>
2023-07-17 15:17:30 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase using putchar function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+
+	putchar('/n');
+
+	return 0;
+}

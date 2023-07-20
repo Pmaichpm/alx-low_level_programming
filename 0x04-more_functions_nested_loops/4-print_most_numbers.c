6c0d116af5d44b32b860c9192e2246da2b54098b
Pmaichpm <pmaich.pm@gmail.com>
2023-07-20 13:13:24 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_most_numbers -  prints the numbers, from 0 to 9
- * Return: void
- */
+* print_most_numbers -  prints the numbers, from 0 to 9
+* Return: void
+*/
 
 void print_most_numbers(void)
 {

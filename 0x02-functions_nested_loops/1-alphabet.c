14121a34f00187e858dd22637e01100e98692294
Pmaichpm <pmaich.pm@gmail.com>
2023-07-24 10:52:09 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ *
+ * Return: 0
+ */
+
+int print_alphabet(void)
+{
+	char a;
+
+	a = 'a';
+
+	while
+		(a <= 'z');
+	{
+		_putchar(a);
+		a++
+	}
+	_putchar('\n')
+
+}

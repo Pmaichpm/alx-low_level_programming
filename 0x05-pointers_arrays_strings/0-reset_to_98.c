9a66f81bbd5803eab084578826070ae8e6c6da8d
Pmaichpm <pmaich.pm@gmail.com>
2023-07-24 16:00:51 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes pointer to an int as parameter and updates the value
+ * @n: puntero
+ * Return: (0)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

45121486635869c5a67993f9763e3d5947d1064a
Pmaichpm <pmaich.pm@gmail.com>
2023-07-24 16:51:56 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*_strlen - returns the length of a string
+*@str: value of length of string
+*Return: (0)
+*/
+
+void int _strlen(char *str)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}

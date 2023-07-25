fdc934a6b45d01d2ac5ee0cf63641df470b798c1
Pmaichpm <pmaich.pm@gmail.com>
2023-07-24 18:31:48 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *puts2 - prints every other character of a string starts with first character
+ *@str: string to be evaluated
+ *Return: void
+ */
+
+void puts2(char *str)
+{
+	int x;
+	int z;
+
+	while (str[z] != '\0')
+	{
+	z++;
+	}
+	for (x = 0 ; x < z; x += 2)
+	{
+	_putchar(str[x]);
+	}
+	_putchar('\n');
+}

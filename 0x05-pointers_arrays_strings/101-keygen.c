f71f3804d143ab7bacb42ca15dc64d1f95bfe826
Pmaichpm <pmaich.pm@gmail.com>
2023-07-25 15:56:42 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates a random password
+ *
+ * Return: generated password
+ */
+
+int main(void)
+{
+	char a;
+	int i;
+
+	srand(time(0));
+	while (i <= 2645)
+	{
+	a = rand() % 128;
+	i += a;
+	putchar(a);
+	}
+	putchar(2772 - i);
+	return (0);
+}

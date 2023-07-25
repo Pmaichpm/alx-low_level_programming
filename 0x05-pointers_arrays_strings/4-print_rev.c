890313e4c9db0e553bb6b568343d9585eb4526dc
Pmaichpm <pmaich.pm@gmail.com>
2023-07-24 17:51:05 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: value of the string to print
+ * Return: (0)
+ */
+
+void print_rev(char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{
+	}
+	for (l = l - 1; l >= 0; l--)
+	{
+		_putchar(s[l]);
+	}
+	_putchar('\n');
+}

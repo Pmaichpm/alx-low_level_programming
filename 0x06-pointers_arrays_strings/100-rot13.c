7652b22f920d7591a170496485e2da999393338a
Pmaichpm <pmaich.pm@gmail.com>
2023-07-26 15:22:27 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @n: inputs value
+ *
+ * Return: void
+ */
+char *rot13(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		while ((n[i] >= 'a' && n[i] <= 'z') ||
+				(n[i] >= 'A' && n[i] <= 'Z'))
+		{
+			if ((n[i] >= 'a' && n[i] <= 'm') ||
+					(n[i] >= 'A' && n[i] <= 'M'))
+				n[i] += 13;
+			else
+				n[i] -= 13;
+			i++;
+		}
+		i++;
+	}
+	return (n);
+}

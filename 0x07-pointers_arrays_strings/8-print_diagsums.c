fd4fa39005b1293b95b50615663ae308b2bb67af
Pmaichpm <pmaich.pm@gmail.com>
2023-07-31 17:08:52 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints diagonals sum
+ * @a: insert value
+ * @size: insert value
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int n;
+	unsigned int u, x;
+
+	u = 0;
+	x = 0;
+
+	for (n = 0; n < size; n++)
+	{
+		u += a[(size * n) + n];
+		x += a[(size * (n + 1)) - (n + 1)];
+	}
+
+	printf("%d, %d\n", u, x);
+}

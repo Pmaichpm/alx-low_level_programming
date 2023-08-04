5975bfb7f799b89cd5f5a0f6a31b1b8d41d6a0a3
Pmaichpm <pmaich.pm@gmail.com>
2023-08-03 18:12:46 -0700

    int _sqrt_recursion(int n)

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - initial function
+ * @n: inserts value
+ *
+ * Return: void
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (defined_sqrt_recursion(n, 0));
+}
+
+/**
+ * defined_sqrt_recursion - returns the natural square root of a number
+ * @x: insert value
+ * @i: insert value
+ *
+ * Return: void
+ */
+
+int defined_sqrt_recursion(int x, int i)
+{
+	if (i * i > x)
+		return (-1);
+	if (i * i == x)
+		return (i);
+	return (defined_sqrt_recursion(x, i + 1));
+}

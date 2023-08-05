4143090f513f116bdde9c0a14c0abee8cdd0a44b
Pmaichpm <pmaich.pm@gmail.com>
2023-08-04 18:18:23 -0700

    whatsmyname.c

---
+++ 0x0A-argc_argv/whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  writes its name
+ * @argc: insert value
+ * @argv: insert value
+ * Rreturn: void
+ * **/
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}

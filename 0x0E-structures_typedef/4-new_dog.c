da38e41f5e4dfedcfc94d03697c8efc1517d8df3
Pmaichpm <pmaich.pm@gmail.com>
2023-08-14 18:45:24 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: insert value
+ * @age: insert value
+ * @owner: insert value
+ *
+ * Return: void
+ **/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+

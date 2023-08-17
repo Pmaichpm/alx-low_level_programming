dba46efcd37d540d479cd9123955805aeeac5533
Pmaichpm <pmaich.pm@gmail.com>
2023-08-16 18:19:16 -0700

    function_pointers.h

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include<stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: insert value
+ * @f: insert value
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+	
+	else
+	f(name);
+
+}

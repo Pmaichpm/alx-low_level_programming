e40651c9ae548e2d43ced052c217ac633fce3414
Pmaichpm <pmaich.pm@gmail.com>
2023-08-17 18:04:54 -0700

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,12 +5,19 @@
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
+
 void print_numbers(const char *separator, const unsigned int n, ...);
+
 void print_strings(const char *separator, const unsigned int n, ...);
+
 void print_all(const char * const format, ...);
-void print_int(va_list ls, char *s)
-void print_char(va_list ls, char *s)
-void print_str(va_list ls, char *s)
+
+void print_int(char *s, va_list ls);
+
+void print_char(char *s, va_list ls);
+
+void print_str(char *s, va_list ls);
+
 void print_flt(va_list ls, char *s)
 
 #endif

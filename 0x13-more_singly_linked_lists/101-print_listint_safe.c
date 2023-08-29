381d2ecfc7f5454ddb54803068644cbdc5b6faee
Pmaichpm <pmaich.pm@gmail.com>
2023-08-29 16:22:39 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -25,10 +25,7 @@ size_t print_listint_safe(const listint_t *head)
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			return (digit);
-			else if (!head)
-				break;
-
+			break;
 	}
 
 	return (digit);

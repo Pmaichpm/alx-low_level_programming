61ae7d369e94e7742af7286487b956c5dbc98a09
Pmaichpm <pmaich.pm@gmail.com>
2023-08-31 14:58:28 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-*print_binary - onverts a binary number to an unsigned int
-*@n: insert number
-*
-*Return: void
-*/
-
+ * print_binary - print binary representation of a number
+ * @n: insert number
+ */
 void print_binary(unsigned long int n)
 {
 	int i;

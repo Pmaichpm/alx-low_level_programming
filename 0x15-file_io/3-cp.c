5a29d6384302067a546f19e94476e22c34f4d076
Pmaichpm <pmaich.pm@gmail.com>
2023-09-04 15:01:00 -0700

    100-elf_header.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: total arguments passed
+ * @argv: array of pointers
+ *
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[])

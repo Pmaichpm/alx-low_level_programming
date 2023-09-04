f3f66ef355867987a546c06fe08ebddeba571801
Pmaichpm <pmaich.pm@gmail.com>
2023-09-04 14:20:52 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,15 +16,15 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wrt = write(o, text_content, lng);
 
 	if (text_content != NULL)
 		{
 		lng = 0;
 		while (*(text_content + lng) != '\0')
 			lng++;
+			wrt = write(o, text_content, lng);
 
-		else if (wrt == -1)
+		if (wrt == -1)
 		{
 			write(1, "fails", 6);
 			return (-1);

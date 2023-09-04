ac94f59f0183a7b369743705558326080c2ce343
Pmaichpm <pmaich.pm@gmail.com>
2023-09-04 14:38:37 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,8 +17,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 		{
-		lng = 0;
-		for (text_content[lng])
+
+		for (lng = 0; text_content[lng];)
 			lng++;
 		}
 

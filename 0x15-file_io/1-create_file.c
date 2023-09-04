c0ca6ef203f5ec168aad432ecba7d0a9dc0cfb28
Pmaichpm <pmaich.pm@gmail.com>
2023-09-04 14:42:42 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int op, wrt, lng;
+	int op, wrt, lng = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(op, text_content, lng);
 
-		if (wrt == -1 || op == -1)
+		if (op == -1 || wrt == -1)
 
 			return (-1);
 

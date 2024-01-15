7dfca8ff273fc99b32e8fbe6b7d59376c49d6235
Pmaichpm <pmaich.pm@gmail.com>
2024-01-15 12:10:48 -0800

    libdynamic.so

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: insert value
+ * @c: insert value
+ *
+ * Return: void
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else if (*(s + 1) == c)
+		{
+			return (s + 1);
+		}
+		s++;
+	}
+	return (s + 1);
+	return (0);
+}
